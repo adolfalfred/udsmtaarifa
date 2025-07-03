7abc3ed7e65cb35cb3d50807e8be7ceb2cfba177
G-mjuni <geofreyjohn711@gmail.com>
2025-07-03 08:25:32 +0300

    fix: update comment for clarity on setAuthCookie function in api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 // This file is used to create an Axios instance for making API requests.
 // It sets the base URL for the API and allows for setting authentication cookies.
+//The `setAuthCookie` function is used to set the authentication cookie in the Axios instance headers.
 const api = axios.create({
   baseURL: process.env.EXPO_PUBLIC_DB_SERVER,
   withCredentials: true,
