56f00a8cc826b427bdf96810b406b2faf70a9181
G-mjuni <geofreyjohn711@gmail.com>
2025-07-03 04:03:35 +0300

    feat: update comments in api.ts for clarity on Axios instance usage

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+// This file is used to create an Axios instance for making API requests.
+// It sets the base URL for the API and allows for setting authentication cookies.
 const api = axios.create({
   baseURL: process.env.EXPO_PUBLIC_DB_SERVER,
   withCredentials: true,
